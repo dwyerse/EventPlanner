082b8d869e07410d169f88387826c4e5fc957cac
SeanDurban <seandurban10@gmail.com>
2018-01-29 23:49:17 +0000

    Added all linting

--- seleniumTest/stest_homepage.js
+++ seleniumTest/stest_homepage.js
@@ -1,15 +1,15 @@
 var assert = require('assert');
 
 describe('homepage tests', function() {
-    //Set browser to localhost:3000
-    //Note: localhost set as default domain in config file
-    browser.url('/');
-    it('should have the correct title', function () {
-        var title = browser.getTitle();
-        assert.equal(title, 'Homepage');
-    });
-    it('Should have welcome message', function () {
-        var welcomeText = browser.getText('#welcome');
-        assert.equal(welcomeText, 'Welcome to our site!');
-    });
+//Set browser to localhost:3000
+//Note: localhost set as default domain in config file
+	browser.url('/');
+	it('should have the correct title', function() {
+		var title = browser.getTitle();
+		assert.equal(title, 'Homepage');
+	});
+	it('Should have welcome message', function() {
+		var welcomeText = browser.getText('#welcome');
+		assert.equal(welcomeText, 'Welcome to our site!');
+	});
 });
--- test/test_server.js
+++ test/test_server.js
@@ -1,44 +1,46 @@
-var assert = require('assert');
-var request = require('request');
+var assert = require("assert");
+var request = require("request");
 
-describe('Server testing suite', function() {
-	it('test if localhost is running', function(done) {
-		request('http://localhost:3000', function (error, response, body) {
-			assert.equal(error, null);
-			assert.equal(response.statusCode, 200);
-			done();
-		});
-	});
+describe("Server testing suite", function() {
+  it("test if localhost is running", function(done) {
+    request("http://localhost:3000", function(error, response, body) {
+      assert.equal(error, null);
+      assert.equal(response.statusCode, 200);
+      done();
+    });
+  });
 });
 
-describe('db testing suite', function() {
-	const MongoClient = require('mongodb').MongoClient;
-	const url = 'mongodb://localhost:27017';
+describe("db testing suite", function() {
+  const MongoClient = require("mongodb").MongoClient;
+  const url = "mongodb://localhost:27017";
   var client;
-	
-	it('should connect to database', function(done) {
-		MongoClient.connect(url, function(err, cl) {
-			assert.equal(null, err);
-			client =cl;
-			done();
-		});
-	});
 
-	it('should be able to insert data', function(done) {
-			var db = client.db("mydb");
-			var myobj = { name: "Company Inc", address: "Highway 37" };
-			db.collection("customers").insertOne(myobj, function(err, res) {
-				assert.equal(null, err);
-				done();
-			});
-	});
+  it("should connect to database", function(done) {
+    MongoClient.connect(url, function(err, cl) {
+      assert.equal(null, err);
+      client = cl;
+      done();
+    });
+  });
 
-	it('should be able to find data', function(done) {
-			var db = client.db("mydb");
-			db.collection("customers").findOne({name:"Company Inc"}, function(err, result) {
-				assert.equal(null, err);
-				assert.equal(result.address, "Highway 37");
-				done();
-			});
-	});
+  it("should be able to insert data", function(done) {
+    var db = client.db("mydb");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    db.collection("customers").insertOne(myobj, function(err, res) {
+      assert.equal(null, err);
+      done();
+    });
+  });
+
+  it("should be able to find data", function(done) {
+    var db = client.db("mydb");
+    db
+      .collection("customers")
+      .findOne({ name: "Company Inc" }, function(err, result) {
+        assert.equal(null, err);
+        assert.equal(result.address, "Highway 37");
+        done();
+      });
+  });
 });
