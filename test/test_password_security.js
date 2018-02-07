e72691e9e362a2f8a1d3cff37562bf72e716bb11
Luke Agnew <agnewl@tcd.ie>
2018-02-07 14:51:30 +0000

    Added salting and hashing functionality for passwords along with its associated testing file

---
+++ test/test_password_security.js
@@ -0,0 +1,17 @@
+//var assert = require('assert');
+//var request = require('request');
+
+describe('Password security testing suite', function() {
+	it('test if passwords are stored securely', function(done) {
+		//var password = 'testPassword';
+		//var hashPassword = saltHashPassword(password);
+
+		//var hash = hashPassword.passwordHash;
+		//var salt = hashPassword.salt;
+
+
+
+		done();
+
+	});
+});
