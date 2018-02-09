0a12de5b16c9ee3f68ac295ebd196a0c9679b545
SeanDurban <seandurban10@gmail.com>
2018-02-09 01:45:04 +0000

    Added promises to selenium tests

--- seleniumTest/changePassword.js
+++ seleniumTest/changePassword.js
@@ -1,8 +1,6 @@
 var assert = require('assert');
 
 describe('Change Password tests', function() {
-//Set browser to localhost:3000
-//Note: localhost set as default domain in config file
 	browser.url('/edit/password');
 	it('should have the correct title', function() {
 		var title = browser.getTitle();
@@ -10,8 +8,11 @@ describe('Change Password tests', function() {
 	});
 
 	it('Should have form POST to /edit/password', function() {
-		var postUrl = browser.getAttribute('#form', 'action');
-		assert.equal(browser.getAttribute('#form', 'method'), 'post');
-		return assert.equal(postUrl, 'http://localhost:3000/edit/password');
+		return new Promise(function(resolve) {
+			var postUrl = browser.getAttribute('#form', 'action');
+			assert.equal(browser.getAttribute('#form', 'method'), 'post');
+			assert.equal(postUrl, 'http://localhost:3000/edit/password');
+			resolve();
+		});
 	});
 });
--- seleniumTest/editAcc.js
+++ seleniumTest/editAcc.js
@@ -1,18 +1,19 @@
 var assert = require('assert');
 
 describe('Edit Account tests', function() {
-//Set browser to localhost:3000
-//Note: localhost set as default domain in config file
 	browser.url('/edit/account');
 	it('should have the correct title', function() {
 		var title = browser.getTitle();
 		return assert.equal(title, 'Edit Account');
 	});
 
-	it('Should have form POST to /edit/account', function() {
-		var postUrl = browser.getAttribute('#form', 'action');
-		var formMethod = browser.getAttribute('#form', 'method');
-		assert.equal(formMethod, 'post');
-		return assert.equal(postUrl, 'http://localhost:3000/edit/account');
+	it('should have form POST to /edit/account', function() {
+		return new Promise(function(resolve) {
+			var postUrl = browser.getAttribute('#form', 'action');
+			var formMethod = browser.getAttribute('#form', 'method');
+			assert.equal(formMethod, 'post');
+			assert.equal(postUrl, 'http://localhost:3000/edit/account');
+			resolve();
+		});
 	});
 });
