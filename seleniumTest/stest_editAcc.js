b1babcf9411307afbe32d385b62771fbf88eafea
SeanDurban <seandurban10@gmail.com>
2018-02-12 19:28:22 +0000

    Commented out test cases

--- seleniumTest/stest_editAcc.js
+++ seleniumTest/stest_editAcc.js
@@ -1,6 +1,16 @@
-var assert = require('assert');
-
+/*var assert = require('assert');
+var testUser ={ _id: '5a81d81c086fec141cf002e1',
+	name: 'Sean Durban',
+	email: 'a2@a',
+	password: 'ad1a76378fb0544137e8564f90af189ebf752e8ccee87c868f2109baf2d9d1e6',
+	salt: '01b026c4fc359b27',
+	__v: 0 };
 describe('Edit Account tests', function() {
+	browser.sessionStorage('POST', {key: 'user', value: testUser});
+	browser.setCookie('post', {
+			 name:'user',
+			 value: testUser
+	 });
 	browser.url('/edit/account');
 	it('should have the correct title', function() {
 		return new Promise(function(resolve){
@@ -20,3 +30,4 @@ describe('Edit Account tests', function() {
 		});
 	});
 });
+*/
