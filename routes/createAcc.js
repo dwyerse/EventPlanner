cd54f48f3ad97e743ee3daacfec78fee18daf778
Luke Agnew <agnewl@tcd.ie>
2018-02-15 22:48:05 +0000

    Fixed createAcc.js errors and updated it to redirect to login page after successfully creating account otherwise it redirects to the create account page.

--- routes/createAcc.js
+++ routes/createAcc.js
@@ -13,37 +13,38 @@ router.get('/admin', function(req, res) {
 router.post('/admin', function(req,res){
 
 	if (validUserParams(req.body)) {
-		if (req.body.inputPassword == req.body.confirmPassword) {
+		if (req.body.inputPassword === req.body.inputConfirmPassword) {
 			var {hash, salt} = hashing.createHash(req.body.inputPassword);
 
 			var userType = 'admin';
 
 			userMapper.addUser(req.body.inputName, req.body.inputEmail, hash, userType, salt, function(error, result) {
 				if (!result) {
-					res.flash('err', 'User could not be created');
+					req.flash('err', 'User could not be created');
 				} else if (error) {
-					res.flash('err', error);
+					req.flash('err', error);
 				} else {
-					res.flash('succ', 'Successfully created new admin account!');
+					req.flash('succ', 'Successfully created new admin account!');
 				}
 
-				res.redirect('/admin');
+				res.redirect('/login');
 			});
 		}
 		else
 		{
-			res.flash('err', 'Passwords do not match');
-			res.redirect('/admin');
+			req.flash('err', 'Passwords do not match');
+			res.redirect('/create/admin');
+			
 		}
 	} else {
-		res.flash('err', 'Not all details provided');
-		res.redirect('/admin');
+		req.flash('err', 'Not all details provided');
+		res.redirect('/create/admin');
 	}
 });
 
 //Validate all correct params included in POST
 function validUserParams(body) {
-	return (body.inputName && body.inputEmail && body.inputPassword && body.confirmPassword);
+	return (body.inputName && body.inputEmail && body.inputPassword && body.inputConfirmPassword);
 }
 
 
