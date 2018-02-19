caff891dfc4a264595f85c2558f3c77b42f11b4e
Luke Agnew <agnewl@tcd.ie>
2018-02-19 17:51:05 +0000

    Added function to config/utils.js which checks whether the current user has admin access. Updated the landingPage to have a button which takes you to the admin access page (if you already have admin access). Fixed errors in routes/adminAccess.js related to using the return value of the findUserByEmail function. Changed heading of views/adminAccess.ejs

--- routes/adminAccess.js
+++ routes/adminAccess.js
@@ -2,37 +2,40 @@ var express = require('express');
 var router = express.Router();
 var userMapper = require('../mappers/userMapper');
 var isLoggedIn = require('../config/utils').isLoggedIn;
+var isAdminUser = require('../config/utils').isAdminUser;
 
-router.get('/admin',isLoggedIn, function(req, res) {
+router.get('/admin',isLoggedIn, isAdminUser, function(req, res) {
 	res.render('adminAccess', { user: req.user,err: req.flash('err'),succ: req.flash('succ')});
 });
 
-router.post('/admin', isLoggedIn, function(req, res) {
+router.post('/admin', isLoggedIn, isAdminUser, function(req, res) {
 	if(validEmail(req.body)){
-		var user = userMapper.findUserByEmail(req.user.email, function(error, result) {
+		userMapper.findUserByEmail(req.body.userEmail, function(error, result) {
 			if (!result) {
 				req.flash('err', 'User email not found');
+				res.redirect('/');
 			} else if (error) {
 				req.flash('err', error);
 			} else {
-				req.flash('succ', 'Successfully found user!');
+
+				var adminType = 'admin';
+				var adminGrantedUser =  {name: result[0].name, email: result[0].email, password:result[0].password, type: adminType, salt:result[0].salt};
+		
+				userMapper.updateUserByEmail(result[0].email, adminGrantedUser, function(error,result) {
+					if (!result) {
+						req.flash('err', 'User email not found');
+					} else if (error) {
+						req.flash('err', error);
+					} else {
+						//We need to update the passport stored user here with 'result'
+						req.flash('succ', 'Successfully granted admin access!');
+					}
+					res.redirect('/grant/admin');
+				});
 			}
+
 		});
 
-		var adminType = 'admin';
-		var adminGrantedUser =  {name: user.name, email: user.email, password:user.password, type: adminType, salt:user.salt};
-		
-		userMapper.updateUserByEmail(user.email, adminGrantedUser, function(error,result) {
-			if (!result) {
-				req.flash('err', 'User email not found');
-			} else if (error) {
-				req.flash('err', error);
-			} else {
-				//We need to update the passport stored user here with 'result'
-				req.flash('succ', 'Successfully granted admin access!');
-			}
-			res.redirect('/grant/admin');
-		});
 	} else {
 		req.flash('err', 'Not all details provided');
 		res.redirect('/grant/admin');
