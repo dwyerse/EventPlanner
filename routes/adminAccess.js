afc9ea5cb21c225c708890b4d8a36608f804d62a
Luke Agnew <agnewl@tcd.ie>
2018-02-20 20:45:46 +0000

    Added function to check if user object returned is empty.

--- routes/adminAccess.js
+++ routes/adminAccess.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var userMapper = require('../mappers/userMapper');
 var isLoggedIn = require('../config/utils').isLoggedIn;
 var isAdminUser = require('../config/utils').isAdminUser;
+var hasOwnProperty = Object.prototype.hasOwnProperty;
 
 router.get('/admin',isLoggedIn, isAdminUser, function(req, res) {
 	res.render('adminAccess', { user: req.user,err: req.flash('err'),succ: req.flash('succ')});
@@ -11,7 +12,7 @@ router.get('/admin',isLoggedIn, isAdminUser, function(req, res) {
 router.post('/admin', isLoggedIn, isAdminUser, function(req, res) {
 	if(validEmail(req.body)){
 		userMapper.findUserByEmail(req.body.userEmail, function(error, result) {
-			if (!result) {
+			if (!result || isEmpty(result)) {
 				req.flash('err', 'User email not found');
 				res.redirect('/');
 			} else if (error) {
@@ -41,6 +42,26 @@ router.post('/admin', isLoggedIn, isAdminUser, function(req, res) {
 		res.redirect('/grant/admin');
 	}
 });
+
+function isEmpty(obj) {
+	// null and undefined are "empty"
+	if (obj == null) return true;
+
+	// Assume if it has a length property with a non-zero value
+	// that that property is correct.
+	if (obj.length > 0)    return false;
+	if (obj.length === 0)  return true;
+
+	if (typeof obj !== 'object') return true;
+
+	// Does it have any properties of its own?
+	for (var key in obj) {
+		if (hasOwnProperty.call(obj, key)) return false;
+	}
+
+	return true;
+}
+
 //Validate all correct params included in POST
 function validEmail(body){
 	return (body.userEmail);
