f225dd5d85c5b6290ad94ace08e2907536002a64
Luke Agnew <agnewl@tcd.ie>
2018-02-20 20:23:58 +0000

    Corrected linting errors in routes/registerGuest.js.

--- routes/registerGuest.js
+++ routes/registerGuest.js
@@ -18,10 +18,10 @@ router.post('/register/guest/:event_id',isLoggedIn, isAdminUser, function(req, r
 	if(validUpdateParams(req.body)){
 
 		var guestEmail = req.body.guestEmail;
-		var guestAccessRequirements = req.body.accessRequirements;
-		var guestDietaryRestrictions = req.body.dietaryRestrictions;
+		//var guestAccessRequirements = req.body.accessRequirements;
+		//var guestDietaryRestrictions = req.body.dietaryRestrictions;
 
-		var currentEventId = req.params.event_id
+		var currentEventId = req.params.event_id;
 
 		eventMapper.findEventBy_event_id(currentEventId, function(err, result) {
 			if (!result) {
@@ -37,18 +37,18 @@ router.post('/register/guest/:event_id',isLoggedIn, isAdminUser, function(req, r
 				var updatedEventObj = new EventModel({title:result[0].title,location:result[0].location,date:result[0].date,description:result[0].description,event_id:result[0].event_id,creators:[],invitees:updatedInvitees});
 				//We corrupt the invitees and creator array here by resetting it to empty
 				eventMapper.updateEventDetailsBy_event_id(result[0].event_id,updatedEventObj,
-				function(error,result) {
-					if (!result) {
-						req.flash('err', 'Event not updated');
-					} else if (error) {
-						req.flash('err', error);
-					} else{
-						req.flash('succ', 'Succesfully registered guest');
-						return res.redirect('/event/view/'+ result[0].event_id);
+					function(error,result) {
+						if (!result) {
+							req.flash('err', 'Event not updated');
+						} else if (error) {
+							req.flash('err', error);
+						} else{
+							req.flash('succ', 'Succesfully registered guest');
+							return res.redirect('/event/view/'+ result[0].event_id);
+						}
+						res.redirect('/event/view' + req.params.event_id);
 					}
-					res.redirect('/event/view' + req.params.event_id);
-				});
-
+				);
 			}
 		});
 
