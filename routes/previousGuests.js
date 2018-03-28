99d89ab4ac6acb7c9b035a9d56201dfcc45dec01
Luke Agnew <agnewl@tcd.ie>
2018-03-28 21:57:59 +0100

    Added code to check for errors from the callbacks in routes/previousGuests.js. Added code in front end to display message to say there are no previous guests if the result length is not greater than zero.

--- routes/previousGuests.js
+++ routes/previousGuests.js
@@ -18,36 +18,38 @@ router.get('/guests',isLoggedIn, isAdminUser, function(req, res) {
 
 		eventMapper.findAllEvents(function(events) {
 			paymentMapper.getAllPayments(function(err, payments) {
-				for (var i=0; i < users.length; i++) {
-					var userEvents = [];
-					
-					for (var j=0; j < events.length; j++) {
-						for (var k=0; k < events[j].invitees.length; k++) {
-							if ((users[i].email == events[j].invitees[k].email) && events[j].invitees[k].state == 'Attending')
-							{
-								userEvents.push(events[j].title);
+				if (!err) {
+					for (var i=0; i < users.length; i++) {
+						var userEvents = [];
+						
+						for (var j=0; j < events.length; j++) {
+							for (var k=0; k < events[j].invitees.length; k++) {
+								if ((users[i].email == events[j].invitees[k].email) && events[j].invitees[k].state == 'Attending')
+								{
+									userEvents.push(events[j].title);
+								}
 							}
 						}
-					}
 
-					var amountSpent = 0;
-					var numDonations = 0;
+						var amountSpent = 0;
+						var numDonations = 0;
 
-					for (var l=0; l < payments.length; l++) {
-						if (users[i]._id == payments[l].user_id) {
-							amountSpent = amountSpent + payments[l].amount;
-							numDonations++;
+						for (var l=0; l < payments.length; l++) {
+							if (users[i]._id == payments[l].user_id) {
+								amountSpent = amountSpent + payments[l].amount;
+								numDonations++;
+							}
+						}
+
+						if (amountSpent > 0) {
+							filteredResult.push([users[i].name, users[i].email, userEvents, amountSpent, numDonations]);
 						}
 					}
 
-					if (amountSpent > 0) {
-						filteredResult.push([users[i].name, users[i].email, userEvents, amountSpent, numDonations]);
-					}
+					filteredResult.sort(function(current, next){return next[3] - current[3];});
 				}
 
-				filteredResult.sort(function(current, next){return next[3] - current[3];});
-
-				res.render('previousGuests', {result: filteredResult,err: req.flash('err'),succ: req.flash('succ')});
+				res.render('previousGuests', {result: filteredResult,err: req.flash('err'),succ: req.flash('succ')});	
 			});
 		});
 	});
