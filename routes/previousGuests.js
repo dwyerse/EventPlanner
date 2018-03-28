5908772b882dcf6db709772757c058167f54d53f
Luke Agnew <agnewl@tcd.ie>
2018-03-28 21:38:34 +0100

    Removed the redundant 'eventsAttended' attribute from the user model and updated routes/previousGuests.js so that it no longer uses this attribute to retrieve the events attended by a user, instead using a combinations of the userMapper, eventMapper and paymentMapper to retrive relevant data for previousGuests. Updated routes/registerGuest.js so it no longer uses redundat 'eventsAttended' attribute for users.

--- routes/previousGuests.js
+++ routes/previousGuests.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userMapper = require('../mappers/userMapper');
+var eventMapper = require('../mappers/eventMapper');
 var paymentMapper = require('../mappers/paymentMapper');
 var isLoggedIn = require('../config/utils').isLoggedIn;
 var isAdminUser = require('../config/utils').isAdminUser;
@@ -8,39 +9,46 @@ EventModel = require('../models/event');
 
 
 router.get('/guests',isLoggedIn, isAdminUser, function(req, res) {
-	userMapper.allUsers(function(err,result){
+	userMapper.allUsers(function(err,users){
 		if(err){
 			res.send(err);
 		}
-		
-		var filteredResult = result.filter(function(user){
-			return (user.eventsAttended.length > 0);
-		});
 
-		paymentMapper.getAllPayments(function(err, payments) {
+		var filteredResult = [];
 
-			var finalResult = [];
+		eventMapper.findAllEvents(function(events) {
+			paymentMapper.getAllPayments(function(err, payments) {
+				for (var i=0; i < users.length; i++) {
+					var userEvents = [];
+					
+					for (var j=0; j < events.length; j++) {
+						for (var k=0; k < events[j].invitees.length; k++) {
+							if ((users[i].email == events[j].invitees[k].email) && events[j].invitees[k].state == 'Attending')
+							{
+								userEvents.push(events[j].title);
+							}
+						}
+					}
 
-			for (var i = 0; i < filteredResult.length; i++) {
-				var amountSpent = 0;
-				var numDonations = 0;
+					var amountSpent = 0;
+					var numDonations = 0;
 
+					for (var l=0; l < payments.length; l++) {
+						if (users[i]._id == payments[l].user_id) {
+							amountSpent = amountSpent + payments[l].amount;
+							numDonations++;
+						}
+					}
 
-				for (var j = 0; j < payments.length; j++) {
-					if (filteredResult[i]._id == payments[j].user_id)
-					{
-						amountSpent = amountSpent + payments[j].amount;
-						numDonations++;
+					if (amountSpent > 0) {
+						filteredResult.push([users[i].name, users[i].email, userEvents, amountSpent, numDonations]);
 					}
 				}
 
-				finalResult.push([filteredResult[i], [amountSpent, numDonations]]);
-			}
+				filteredResult.sort(function(current, next){return next[3] - current[3];});
 
-			finalResult.sort(function(current, next){return next[1][0] - current[1][0];});
-
-			res.render('previousGuests', {result: finalResult,err: req.flash('err'),succ: req.flash('succ')});
-		
+				res.render('previousGuests', {result: filteredResult,err: req.flash('err'),succ: req.flash('succ')});
+			});
 		});
 	});
 });
